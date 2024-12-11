177bfcdca82164828890933d7945ec78fde386d4
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-12-12 01:33:31 +0900

    feat: 계좌 및 거래 타입으로 거래내역 찾는 메서드 추가

--- backend/src/main/java/com/hbbank/backend/repository/TransactionRepository.java
+++ backend/src/main/java/com/hbbank/backend/repository/TransactionRepository.java
@@ -6,11 +6,14 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.hbbank.backend.domain.Account;
 import com.hbbank.backend.domain.Transaction;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     public Optional<List<Transaction>> findAllByAccount_IdOrderByTransactionDateTimeDesc(Long accountId);
+
+    public Optional<List<Transaction>> findByAccountAndTransactionType(Account account, String transactionType);
 
 }
