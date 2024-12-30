1da6837e0afa03a00803efdba20e86768d900b57
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-12-30 22:28:39 +0900

    test: TransferService 단위 테스트 시나리오 구현
    - 계좌 잠금 및 유효성 검사를 포함한 이체 성공/실패 시나리오에 대한 새로운 테스트 케이스 추가
    - 계좌 상태 및 이체 한도와 같은 엣지 케이스에 대한 더 나은 커버리지를 위해 기존 테스트 개선
    - 테스트 설정의 간소화와 가독성 향상을 위해 초기화 로직 리팩토링

--- backend/src/test/java/com/hbbank/backend/service/TransferServiceTest.java
+++ backend/src/test/java/com/hbbank/backend/service/TransferServiceTest.java
@@ -1,30 +1,42 @@
 package com.hbbank.backend.service;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
 import com.hbbank.backend.domain.Account;
+import com.hbbank.backend.domain.Transaction;
 import com.hbbank.backend.domain.User;
 import com.hbbank.backend.domain.enums.AccountStatus;
+import com.hbbank.backend.domain.enums.TransferType;
+import com.hbbank.backend.dto.TransferRequestDTO;
+import com.hbbank.backend.exception.*;
+import com.hbbank.backend.repository.AccountRepository;
 import com.hbbank.backend.repository.TransactionRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 /*
  * TransferService 단위 테스트
  */
 @ExtendWith(MockitoExtension.class)
 public class TransferServiceTest {
-
+    @Mock
+    private TransactionRepository transactionRepository;
+    @Mock
+    private AccountRepository accountRepository;
     @Mock
     private TransactionService transactionService;
-    @Mock
-    private AccountService accountService;
     @Mock
     private PasswordEncoder encoder;
 
@@ -33,14 +45,16 @@ public class TransferServiceTest {
 
     private Account fa, ta;
     private BigDecimal amount;
+    private TransferRequestDTO dto1;
+    private TransferRequestDTO dto2;
 
     @BeforeEach
-    @DisplayName("출금 계좌, 입금 계좌, 거래액 생성")
+    @DisplayName("출금 계좌, 입금 계좌, 거래액, 이체 요청 생성")
     void init() {
         fa = Account.builder()
                 .id(1L)
                 .status(AccountStatus.ACTIVE)
-                .accountNumber("0987654321")
+                .accountNumber("9876543210")
                 .password("encodedPassword")
                 .balance(new BigDecimal("5000"))
                 .user(User.builder().id(1L).build())
@@ -62,81 +76,272 @@ public class TransferServiceTest {
                 .build();
 
         amount = new BigDecimal("1000");
+
+        dto1 = TransferRequestDTO.builder()
+                .type(TransferType.INSTANT)
+                .fromAccountId(fa.getId())
+                .toAccountNumber(ta.getAccountNumber())
+                .amount(amount)
+                .password("1234")
+                .build();
+        dto2 = TransferRequestDTO.builder()
+                .type(TransferType.INSTANT)
+                .fromAccountId(ta.getId())
+                .toAccountNumber(fa.getAccountNumber())
+                .amount(amount)
+                .password("1234")
+                .build();
     }
 
     @Test
-    @DisplayName("계좌 락 획득 성공 - 출금 계좌번호 > 입금 계좌번호")
-    void getLock_Success_WithdrawGreaterThanDeposit() {
-        // TODO document why this method is empty
+    @DisplayName("이체 성공")
+    void executeTransfer_Success() {
+        //given
+        when(accountRepository.findById(fa.getId())).thenReturn(Optional.of(fa));
+        when(accountRepository.findByIdWithLock(fa.getId())).thenReturn(Optional.of(fa));
+        when(accountRepository.findByAccountNumberWithLock(ta.getAccountNumber())).thenReturn(Optional.of(ta));
+        when(encoder.matches(any(), any())).thenReturn(true);
+        doNothing().when(transactionService).createTransaction(fa, ta, amount);
+        when(accountRepository.saveAndFlush(any(Account.class))).thenReturn(new Account());
+
+        //when
+        // 이체 실행
+        boolean result = transferService.transfer(dto1);
+
+        //then
+        //결과 및 잔액 검증
+        assertTrue(result);
+        assertEquals(new BigDecimal("4000"), fa.getBalance());
+        assertEquals(new BigDecimal("2000"), ta.getBalance());
+
+        //db 호출 검증
+        verify(accountRepository).findById(any());
+        verify(accountRepository).findByIdWithLock(any());
+        verify(accountRepository).findByAccountNumberWithLock(any());
+        verify(accountRepository, times(2)).saveAndFlush(any());
     }
 
     @Test
-    @DisplayName("계좌 락 획득 성공 - 출금 계좌번호 < 입금 계좌번호")
+    @DisplayName("계좌 락 획득 성공 - 출금 계좌 -> 입금 계좌")
     void getLock_Success_WithdrawLessThanDeposit() {
-        // TODO document why this method is empty
+        //given
+        when(accountRepository.findById(ta.getId())).thenReturn(Optional.of(ta));
+        when(accountRepository.findByAccountNumberWithLock(fa.getAccountNumber())).thenReturn(Optional.of(fa));
+        when(accountRepository.findByIdWithLock(ta.getId())).thenReturn(Optional.of(ta));
+        when(encoder.matches(any(), any())).thenReturn(true);
+        when(accountRepository.saveAndFlush(any(Account.class))).thenReturn(new Account());
+
+        //when
+        boolean result = transferService.transfer(dto2);
+
+        //then
+        InOrder order = inOrder(accountRepository);
+        order.verify(accountRepository).findByIdWithLock(ta.getId());
+        order.verify(accountRepository).findByAccountNumberWithLock(fa.getAccountNumber());
+        assertTrue(result);
     }
 
+    @Test
+    @DisplayName("계좌 락 획득 성공 - 입금 계좌 -> 출금 계좌")
+    void getLock_Success_DepositLessThanWithdraw() {
+        //given
+        when(accountRepository.findById(fa.getId())).thenReturn(Optional.of(fa));
+        when(accountRepository.findByAccountNumberWithLock(ta.getAccountNumber())).thenReturn(Optional.of(fa));
+        when(accountRepository.findByIdWithLock(fa.getId())).thenReturn(Optional.of(ta));
+        when(encoder.matches(any(), any())).thenReturn(true);
+        when(accountRepository.saveAndFlush(any(Account.class))).thenReturn(null);
+
+        //when
+        boolean result = transferService.transfer(dto1);
+
+        //then
+        InOrder order = inOrder(accountRepository);
+        order.verify(accountRepository).findByAccountNumberWithLock(ta.getAccountNumber());
+        order.verify(accountRepository).findByIdWithLock(fa.getId());
+        assertTrue(result);
+    }
+
+    // 계좌 락 획득 실패는 출금 계좌번호가 입금 계좌번호보다 작을 때를 가정함
     @Test
     @DisplayName("계좌 락 획득 실패 - 출금 계좌 없음")
     void getLock_Fail_WithdrawAccountNotFound() {
-        // TODO document why this method is empty
+        //given
+        Long accountId = dto1.getFromAccountId();
+        when(accountRepository.findById(accountId)).thenThrow(new AccountNotFoundException());
+
+        //when&then
+        assertThrows(AccountNotFoundException.class, () -> transferService.transfer(dto1));
+        verify(accountRepository).findById(accountId);
+        verify(accountRepository, times(0)).findByIdWithLock(accountId);
+        verify(accountRepository, times(0)).findByAccountNumberWithLock(ta.getAccountNumber());
+        verify(transactionRepository, times(0)).save(any(Transaction.class));
+        verify(accountRepository, times(0)).saveAndFlush(any(Account.class));
     }
 
     @Test
     @DisplayName("계좌 락 획득 실패 - 입금 계좌 없음")
     void getLock_Fail_DepositAccountNotFound() {
-        // TODO document why this method is empty
-    }
+        //given
+        Long accountId = dto1.getFromAccountId();
+        when(accountRepository.findById(accountId)).thenReturn(Optional.of(fa));
+        when(accountRepository.findByAccountNumberWithLock(ta.getAccountNumber())).thenThrow(new AccountNotFoundException());
 
-    @Test
-    @DisplayName("비밀번호 확인 성공")
-    void checkPassword_Success() {
-        // TODO document why this method is empty
+        //when&then
+        assertThrows(AccountNotFoundException.class, () -> transferService.transfer(dto1));
+        verify(accountRepository).findById(accountId);
+        verify(accountRepository).findByAccountNumberWithLock(ta.getAccountNumber());
+        verify(transactionRepository, times(0)).save(any(Transaction.class));
+        verify(accountRepository, times(0)).saveAndFlush(any(Account.class));
     }
 
     @Test
     @DisplayName("비밀번호 확인 실패 - 비밀번호 불일치")
     void checkPassword_Fail_InvalidPassword() {
-        // TODO document why this method is empty
-    }
+        //given
+        when(accountRepository.findById(fa.getId())).thenReturn(Optional.of(fa));
+        when(accountRepository.findByIdWithLock(fa.getId())).thenReturn(Optional.of(fa));
+        when(accountRepository.findByAccountNumberWithLock(ta.getAccountNumber())).thenReturn(Optional.of(ta));
+        when(encoder.matches(any(), any())).thenReturn(false);
 
-    @Test
-    @DisplayName("이체 실행 성공")
-    void executeTransfer_Success() {
-        // TODO document why this method is empty
-        /*
-        출금 계좌 출금 -> 입금 계좌 입금 -> 거래내여 생성 -> 계좌 저장까지
-         */
+        //when &then
+        assertThrows(InvalidPasswordException.class, () -> transferService.transfer(dto1));
+        verify(accountRepository).findById(fa.getId());
+        verify(accountRepository).findByIdWithLock(fa.getId());
+        verify(accountRepository).findByAccountNumberWithLock(ta.getAccountNumber());
+        verify(transactionRepository, times(0)).save(any(Transaction.class));
+        verify(accountRepository, times(0)).saveAndFlush(any(Account.class));
     }
 
     @Test
     @DisplayName("이체 실행 실패 - 유효하지 않은 출금 계좌 상태")
     void executeTransfer_Fail_InvalidWithdrawAccountStatus() {
-        // TODO document why this method is empty
+        //given
+        Account inactive = Account.builder()
+                .id(fa.getId())
+                .accountNumber(fa.getAccountNumber())
+                .balance(fa.getBalance())
+                .status(AccountStatus.DORMANT)  // 휴면
+                .build();
+
+        when(accountRepository.findById(inactive.getId())).thenReturn(Optional.of(inactive));
+        when(accountRepository.findByIdWithLock(inactive.getId())).thenReturn(Optional.of(inactive));
+        when(accountRepository.findByAccountNumberWithLock(ta.getAccountNumber())).thenReturn(Optional.of(ta));
+        when(encoder.matches(any(), any())).thenReturn(true);
+
+        //when & then
+        assertThrows(InvalidAccountStatusException.class, () -> transferService.transfer(dto1));
+        verify(accountRepository).findById(any());
+        verify(accountRepository).findByIdWithLock(any());
+        verify(accountRepository).findByAccountNumberWithLock(ta.getAccountNumber());
+        verify(transactionRepository, times(0)).save(any(Transaction.class));
+        verify(accountRepository, times(0)).saveAndFlush(any(Account.class));
     }
 
     @Test
     @DisplayName("이체 실행 실패 - 유효하지 않은 입금 계좌 상태")
     void executeTransfer_Fail_InvalidDepositAccountStatus() {
-        // TODO document why this method is empty
+        //given
+        Account inactive = Account.builder()
+                .id(ta.getId())
+                .accountNumber(ta.getAccountNumber())
+                .balance(ta.getBalance())
+                .status(AccountStatus.DORMANT)  // 휴면
+                .build();
+
+        when(accountRepository.findById(fa.getId())).thenReturn(Optional.of(fa));
+        when(accountRepository.findByIdWithLock(fa.getId())).thenReturn(Optional.of(fa));
+        when(accountRepository.findByAccountNumberWithLock(ta.getAccountNumber())).thenReturn(Optional.of(inactive));
+        when(encoder.matches(any(), any())).thenReturn(true);
+
+        //when & then
+        assertThrows(InvalidAccountStatusException.class, () -> transferService.transfer(dto1));
+        verify(accountRepository).findById(any());
+        verify(accountRepository).findByIdWithLock(any());
+        verify(accountRepository).findByAccountNumberWithLock(ta.getAccountNumber());
+        verify(transactionRepository, times(0)).save(any(Transaction.class));
+        verify(accountRepository, times(0)).saveAndFlush(any(Account.class));
     }
 
     @Test
     @DisplayName("이체 실행 실패 - 1회 이체한도 초과")
     void executeTransfer_Fail_TransferLimitExceeded() {
-        // TODO document why this method is empty
+        //given
+        Account inactive = Account.builder()
+                .id(fa.getId())
+                .accountNumber(fa.getAccountNumber())
+                .balance(fa.getBalance())
+                .status(AccountStatus.ACTIVE)
+                .transferLimit(new BigDecimal("0"))
+                .build();
+
+        when(accountRepository.findById(fa.getId())).thenReturn(Optional.of(inactive));
+        when(accountRepository.findByIdWithLock(fa.getId())).thenReturn(Optional.of(inactive));
+        when(accountRepository.findByAccountNumberWithLock(ta.getAccountNumber())).thenReturn(Optional.of(ta));
+        when(encoder.matches(any(), any())).thenReturn(true);
+
+        //when & then
+        assertThrows(TransferLimitExceededException.class, () -> transferService.transfer(dto1));
+        verify(accountRepository).findById(any());
+        verify(accountRepository).findByIdWithLock(any());
+        verify(accountRepository).findByAccountNumberWithLock(ta.getAccountNumber());
+        verify(transactionRepository, times(0)).save(any(Transaction.class));
+        verify(accountRepository, times(0)).saveAndFlush(any(Account.class));
     }
 
     @Test
-    @DisplayName("이체 실행 실패 - 1일 이체한도 초과")
+    @DisplayName("이체 실행 실패 - 일일 이체한도 초과")
     void executeTransfer_Fail_ValidateDailyTransferLimit() {
-        // TODO document why this method is empty
+        //given
+        Account inactive = Account.builder()
+                .id(fa.getId())
+                .accountNumber(fa.getAccountNumber())
+                .balance(fa.getBalance())
+                .status(AccountStatus.ACTIVE)
+                .transferLimit(new BigDecimal("10000"))
+                .dailyTransferLimit(new BigDecimal("0"))
+                .dailyTransferredAmount(new BigDecimal("0"))
+                .build();
+
+        when(accountRepository.findById(fa.getId())).thenReturn(Optional.of(inactive));
+        when(accountRepository.findByIdWithLock(fa.getId())).thenReturn(Optional.of(inactive));
+        when(accountRepository.findByAccountNumberWithLock(ta.getAccountNumber())).thenReturn(Optional.of(ta));
+        when(encoder.matches(any(), any())).thenReturn(true);
+
+        //when & then
+        assertThrows(DailyTransferLimitExceededException.class, () -> transferService.transfer(dto1));
+        verify(accountRepository).findById(any());
+        verify(accountRepository).findByIdWithLock(any());
+        verify(accountRepository).findByAccountNumberWithLock(ta.getAccountNumber());
+        verify(transactionRepository, times(0)).save(any(Transaction.class));
+        verify(accountRepository, times(0)).saveAndFlush(any(Account.class));
     }
 
     @Test
     @DisplayName("이체 실행 실패 - 잔액 부족")
     void executeTransfer_Fail_ValidateBalance() {
-        // TODO document why this method is empty
+        //given
+        Account inactive = Account.builder()
+                .id(fa.getId())
+                .accountNumber(fa.getAccountNumber())
+                .balance(BigDecimal.ZERO)
+                .status(AccountStatus.ACTIVE)
+                .transferLimit(new BigDecimal("10000"))
+                .dailyTransferLimit(new BigDecimal("9999999999"))
+                .dailyTransferredAmount(new BigDecimal("0"))
+                .build();
+
+        when(accountRepository.findById(fa.getId())).thenReturn(Optional.of(inactive));
+        when(accountRepository.findByIdWithLock(fa.getId())).thenReturn(Optional.of(inactive));
+        when(accountRepository.findByAccountNumberWithLock(ta.getAccountNumber())).thenReturn(Optional.of(ta));
+        when(encoder.matches(any(), any())).thenReturn(true);
+
+        //when & then
+        assertThrows(OutofBalanceException.class, () -> transferService.transfer(dto1));
+        verify(accountRepository).findById(any());
+        verify(accountRepository).findByIdWithLock(any());
+        verify(accountRepository).findByAccountNumberWithLock(ta.getAccountNumber());
+        verify(transactionRepository, times(0)).save(any(Transaction.class));
+        verify(accountRepository, times(0)).saveAndFlush(any(Account.class));
     }
 
 }
